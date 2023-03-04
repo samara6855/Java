2257a02f81b8569f9d19a670c77bf369e843db01
samara6855 <101248119+manojreddy6855@users.noreply.github.com>
2023-03-04 15:06:30 +0530

    Program demonstrating the datatypes in java

---
+++ Datatypes.java
@@ -0,0 +1,19 @@
+public class Datatypes{
+	public static void main(String[] args)
+	{
+		String name = "India";
+		int population= 132962000;
+	    float pDensity= 393.0f;
+		double GDP=2.454d;
+		char currency = 'R';
+		boolean isSecure= true;
+		System.out.println(name);
+		System.out.println(population);
+		System.out.println(pDensity);
+		System.out.println(GDP);
+		System.out.println(currency);
+		System.out.println(isSecure);
+		System.out.println("That's all of the datatypes");
+
+	}
+}
