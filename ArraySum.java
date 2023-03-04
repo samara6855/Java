54ddd0a552e5a383c76116f2e103fb0c040fc4a2
samara6855 <101248119+manojreddy6855@users.noreply.github.com>
2023-03-04 15:09:11 +0530

    Program to print the sum of elements in an Array.

---
+++ ArraySum.java
@@ -0,0 +1,24 @@
+public class ArraySum{
+
+	public static void main(String[] args) {
+
+		// 2D array is collection of 1D Array
+
+		int[][] myFirst2DArray = {
+				{ 3, 5, 1, 9 },
+				{ 10, 15, 3, 0  },
+				{ 1, 11, 31, 90 },
+				{ 2, 51, 1, 9 }
+		};
+		int sum=0;
+
+		for (int row = 0; row < myFirst2DArray.length; row++) {
+
+			for (int column = 0; column < myFirst2DArray.length; column++) {
+				sum +=myFirst2DArray[row][column];
+			}
+
+			System.out.println(sum);
+		}
+	}
+}
