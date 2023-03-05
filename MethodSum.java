09a723de61a28ecd65deb18df0a6e4c79c54c9e9
samara6855 <101248119+manojreddy6855@users.noreply.github.com>
2023-03-05 16:48:08 +0530

    Program to give sum using methods.

---
+++ MethodSum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int x=sc.nextInt();
+		System.out.println("Enter second number: ");
+		int y=sc.nextInt();
+		int c = Sum(x,y);
+		System.out.println("The sum of give two integers is: "+c);
+	}
+	public static int Sum(int a,int b){
+		int c=a+b;
+		return c;
+	}
+}
