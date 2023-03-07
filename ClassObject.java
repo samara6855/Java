538d04bcff6be2abc411b6b6091992142498da1c
samara6855 <101248119+samara6855@users.noreply.github.com>
2023-03-07 16:50:08 +0530

    Program to demonstrate 'class' and 'objects'.
    Here, we have created a class named 'Cake' and their objects, and we have used them in our 'main' method.

---
+++ ClassObject.java
@@ -0,0 +1,23 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Cake c = new Cake(); // 'c' is reference variable
+		c.cakeName = "Black Forest";    
+		c.price = 55.0f;                
+		c.favCake();
+	}
+
+	public static class Cake {
+		String cakeName; //instance variable
+		float price; //instance variable
+		String flavour; //instance variable
+		boolean isAvailable; //instance variable
+
+		public String favCake() {
+      int layers=4; //local variable
+			System.out.println("My favourite cake is : " + cakeName + " with " + layers +" layers.");
+			return null;
+		}
+
+	}
+}
