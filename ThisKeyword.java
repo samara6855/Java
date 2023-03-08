0f689792aa66a7b32fc014c29f3d727c3aab2ccd
samara6855 <101248119+samara6855@users.noreply.github.com>
2023-03-08 20:47:00 +0530

    Program to demonstrate 'this' keyword.
    Using "Contructors".

---
+++ ThisKeyword.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+		Rectangle r= new Rectangle();
+		Rectangle r1=new Rectangle(5.6f,10.7f);
+	}
+}
+class Rectangle{
+	float length;
+	float breadth;
+	public Rectangle(){
+		System.out.println("This is default constructor");//default constructor
+	}
+	public Rectangle(float length, float breadth){     //parameterized constructor
+		this.length=length;
+		this.breadth=breadth;
+		System.out.println("The length of the rectangle is "+length);
+		System.out.println("The breadth of the rectangle is "+ breadth);
+		}
+	}
