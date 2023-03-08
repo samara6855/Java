32b9a0d8af0124368e6eee8c0cc8b1c6045c5e4c
samara6855 <101248119+samara6855@users.noreply.github.com>
2023-03-08 20:39:56 +0530

    Program to show the working of "Constructors"
    Both the "default" and "parameterized" constructors.

---
+++ Constructors.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+		Rectangle r= new Rectangle();
+		Rectangle r1=new Rectangle(5.6f,10.7f);
+	}
+}
+class Rectangle{
+	float length;
+	float breadth;
+	public Rectangle(){
+		System.out.println("This is default constructor");//default constructor
+	}
+	public Rectangle(float l,float b){     //parameterized constructor
+		length=l;
+		breadth=b;
+		System.out.println("The length of the rectangle is "+length);
+		System.out.println("The breadth of the rectangle is "+ breadth);
+
+	}
+}
