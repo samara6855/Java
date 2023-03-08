2dd89c9361608344dc8ac421a72067e45917be87
samara6855 <101248119+samara6855@users.noreply.github.com>
2023-03-08 21:43:12 +0530

    Creating "abstract" class and method in java.
    Using "abstract" keyword to create class and method in Java.

---
+++ AbstractKeyword.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+		Animal animal=new Dog(); //UPCASTING
+		animal.eat();
+		animal.run();
+	}
+}
+abstract class Animal{
+	public void run(){
+		System.out.println("Animal is running!!!!!");
+	}
+	abstract void eat();
+}
+class Dog extends Animal{
+	@Override
+	public void eat(){
+		System.out.println("I'm eating!!");
+	}
+}
