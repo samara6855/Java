9d62c08337217570f866a20ebf73efdae15c6d4c
samara6855 <101248119+samara6855@users.noreply.github.com>
2023-03-09 21:57:30 +0530

    Program to "Read from a file" in java.

---
+++ FileRead.java
@@ -0,0 +1,17 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			FileInputStream fis = new FileInputStream("file1.txt");
+			int i=0;
+			while((i=fis.read())!= -1){
+				System.out.print((char) i);
+			}
+			fis.close();
+		} catch (IOException e){
+			System.out.println("There is an error!!!!!");
+		}
+	}
+}
