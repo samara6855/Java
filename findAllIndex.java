45a6f8fa9121bd1aea6f96ba85b894f176d641a0
Yasani Samara Simha Reddy <101248119+samara6855@users.noreply.github.com>
2023-06-16 21:08:48 +0530

    Linear Search (Finding all matching indexes)

---
+++ findAllIndex.java
@@ -0,0 +1,23 @@
+package com.simha.java;
+
+import java.util.ArrayList;
+
+public class findAllIndex {
+    public static void main(String[] args) {
+        int[] arr={1,4,5,8,5,15,25};
+        int target = 5;
+
+        System.out.println(findAllIndex(arr,target,0,new ArrayList<>()));
+    }
+
+    static ArrayList findAllIndex(int[] arr,int target,int index,ArrayList<Integer> list){
+        if(index==arr.length-1){
+            return list;
+        }
+        if(target==arr[index]){
+            list.add(index);
+        }
+
+        return findAllIndex(arr,target,index+1,list);
+    }
+}
