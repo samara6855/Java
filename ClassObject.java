a11de42ef6735402139f7ebbb7dae93bc5a40c30
Yasani Samara Simha Reddy <101248119+samara6855@users.noreply.github.com>
2023-08-11 21:22:18 +0530

    Update ClassObject.java

--- ClassObject.java
+++ ClassObject.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cake c = new Cake(); //reference variable
+		Cake c = new Cake(); // Reference variable
 		c.cakeName = "Black Forest";    
 		c.price = 55.0f;                
 		c.favCake();
