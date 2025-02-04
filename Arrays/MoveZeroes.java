840cb7e5405356ae89b4621a532945db12d14c41
Samara Simha Reddy Yasani <101248119+samara6855@users.noreply.github.com>
2025-02-04 17:15:23 +0530

    Create MoveZeroes.java

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/move-zeroes/
+class Solution {
+    // Brute Force - Store non zero elements then again store in main array and fill zeroes next
+    public static int[] moveZeros(int n, int []a) {
+        //temporary array:
+        ArrayList<Integer> temp = new ArrayList<>();
+        //copy non-zero elements
+        //from original -> temp array:
+        for (int i = 0; i < n; i++) {
+            if (a[i] != 0)
+                temp.add(a[i]);
+        }
+
+        // number of non-zero elements.
+        int nz = temp.size();
+
+        //copy elements from temp
+        //fill first nz fields of
+        //original array:
+        for (int i = 0; i < nz; i++) {
+            a[i] = temp.get(i);
+        }
+
+        //fill rest of the cells with 0:
+        for (int i = nz; i < n; i++) {
+            a[i] = 0;
+        }
+        return a;
+    }
+  
+    // Optimal - j index is the beginning index, and i finds non zero elements and then swapping occurs. Time: O(N), Space: O(1)
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+
+        }
+    }
+}
