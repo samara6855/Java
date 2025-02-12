d84dcb2d4077c12b6c0947eaba4ee5e385dcb125
Samara Simha Reddy Yasani <101248119+samara6855@users.noreply.github.com>
2025-02-12 16:38:04 +0530

    Update MergeOverlappingSubIntervals.java

--- Arrays/MergeOverlappingSubIntervals.java
+++ Arrays/MergeOverlappingSubIntervals.java
@@ -31,6 +31,18 @@ class Solution {
         return result;
     }
 
-    // Optimal -
-    
+    // Optimal - T.C: O(nlogn)+O(n), S.C: O(n)
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int k = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[k][1] >= intervals[i][0]) {
+                intervals[k][1] = Math.max(intervals[k][1], intervals[i][1]);
+            } else {
+                k++;
+                intervals[k] = intervals[i];
+            }
+        }
+        return Arrays.copyOfRange(intervals, 0, k + 1);
+    }
 }
