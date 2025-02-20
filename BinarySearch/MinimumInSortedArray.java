e159d3697b0446b50386bf2ff5fe88711d62bb61
Samara Simha Reddy Yasani <101248119+samara6855@users.noreply.github.com>
2025-02-20 21:53:03 +0530

    Update MinimumInSortedArray.java

--- BinarySearch/MinimumInSortedArray.java
+++ BinarySearch/MinimumInSortedArray.java
@@ -22,4 +22,28 @@ class Solution {
         }
         return ans;
     }
+
+    // More optimal - If the search space is sorted, which means arr[low]<=arr[high], arr[low] will be the answer
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            if(nums[low]<=nums[high]){
+                ans = Math.min(ans,nums[low]);
+                break;
+            }    
+            
+            if(nums[low]<=nums[mid]){
+                ans = Math.min(ans,nums[low]);
+                low = mid+1;
+            }else{
+                ans = Math.min(ans,nums[mid]);
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
 }
