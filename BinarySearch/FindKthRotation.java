ef0c36c8253a9b88ad9f09a37d34dc90b8451639
Samara Simha Reddy Yasani <101248119+samara6855@users.noreply.github.com>
2025-02-20 22:22:58 +0530

    Create FindKthRotation.java

---
+++ BinarySearch/FindKthRotation.java
@@ -0,0 +1,61 @@
+// https://www.geeksforgeeks.org/problems/rotation4723/1
+
+class Solution {
+  
+  // Brute Force - Direct Search For Smallest Element and Return its Index - O(n)
+  public static int findKRotation(int[] arr) {
+        int n = arr.length; //size of array.
+        int ans = Integer.MAX_VALUE, index = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < ans) {
+                ans = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+  
+  // Optimal - O(logn)
+  public static int findKRotation(int[] arr) {
+        int low = 0, high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        int index = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            //search space is already sorted
+            //then arr[low] will always be
+            //the minimum in that search space:
+            if (arr[low] <= arr[high]) {
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+                break;
+            }
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                // keep the minimum:
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+
+                // Eliminate left half:
+                low = mid + 1;
+            } else { //if right part is sorted:
+
+                // keep the minimum:
+                if (arr[mid] < ans) {
+                    index = mid;
+                    ans = arr[mid];
+                }
+
+                // Eliminate right half:
+                high = mid - 1;
+            }
+        }
+        return index;
+    }
+}
+
