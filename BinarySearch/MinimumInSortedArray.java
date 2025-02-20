f46133953a1e1214c10dcf44ef824e406ae2aecb
Samara Simha Reddy Yasani <101248119+samara6855@users.noreply.github.com>
2025-02-20 21:48:51 +0530

    Create MinimumInSortedArray.java

---
+++ BinarySearch/MinimumInSortedArray.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+class Solution {
+
+    // Brute Force - Finding minimum using loop in the array - O(n)
+
+    // Optimal - Finding the sorted part and taking minimum, and then removing that part and check on the remaining part
+    // O(logn)
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[low]<=nums[mid]){
+                ans = Math.min(ans,nums[low]);
+                low = mid+1;
+            }else{
+                ans = Math.min(ans,nums[mid]);
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+}
